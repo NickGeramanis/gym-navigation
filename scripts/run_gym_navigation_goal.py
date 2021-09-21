df0685cb7380cc1cdd6666c9b1ee8bc2649b7ed0
NickGer <nickgeramanis@gmail.com>
2021-09-21 11:13:56 +0300

    removed sonarcloud intergration

---
+++ scripts/run_gym_navigation_goal.py
@@ -0,0 +1,15 @@
+import gym
+
+env_name = 'gym_navigation:NavigationGoal-v0'
+env = gym.make(env_name, track_id=1)
+
+for i_episode in range(10):
+    done = False
+    env.reset()
+
+    while not done:
+        env.render()
+        action = env.action_space.sample()
+        observation, reward, done, info = env.step(action)
+
+env.close()
