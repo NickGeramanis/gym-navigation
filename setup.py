b8b7ea40569abbbac56783e484280f59abe9346a
NickGer <nickgeramanis@gmail.com>
2021-09-24 20:53:54 +0300

    Small update in setup file

--- setup.py
+++ setup.py
@@ -1,20 +1,13 @@
 from setuptools import setup
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
 setup(name='gym_navigation',
       version='0.1.0',
+      packages=['gym_navigation',
+                'gym_navigation.envs',
+                'gym_navigation.utils'],
       author='Nick Geramanis',
       author_email='nickgeramanis@gmail.com',
       description='Navigation Environment for OpenAI Gym',
-      long_description=long_description,
-      long_description_content_type="text/markdown",
       url='https://github.com/NickGeramanis/gym-navigation',
-      project_urls={
-          "Bug Tracker": "https://github.com/NickGeramanis/gym-navigation"
-                         "/issues"
-      },
       license='GPLV3',
-      install_requires=['gym', 'numpy', 'matplotlib']
-      )
+      install_requires=['gym', 'numpy', 'matplotlib'])
