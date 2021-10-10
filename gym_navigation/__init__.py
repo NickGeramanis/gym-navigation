cfed777ec28468e2a56842e106f97214e2ddd047
NickGer <nickgeramanis@gmail.com>
2021-10-10 21:50:39 +0300

    Small refactoring

--- gym_navigation/__init__.py
+++ gym_navigation/__init__.py
@@ -2,8 +2,8 @@
 from gym.envs.registration import register
 
 register(
-    id='Navigation-v0',
-    entry_point='gym_navigation.envs.navigation_env:NavigationEnv')
+    id='NavigationTrack-v0',
+    entry_point='gym_navigation.envs.navigation_track:NavigationTrack')
 register(
     id='NavigationGoal-v0',
-    entry_point='gym_navigation.envs.navigation_goal_env:NavigationGoalEnv')
+    entry_point='gym_navigation.envs.navigation_goal:NavigationGoal')
