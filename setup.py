db5b078d3179eeeb96f85e7da12dc0b12375cdd9
nickg <nickgeramanis@gmail.com>
2022-07-09 13:13:52 +0300

    minor correction

--- setup.py
+++ setup.py
@@ -10,6 +10,6 @@ setup(name='gym_navigation',
       description='Navigation Environment for OpenAI Gym',
       url='https://github.com/NickGeramanis/gym-navigation',
       license='GPLV3',
-      python_requires='>3.10.5',
+      python_requires='==3.10.5',
       install_requires=['gym==0.24.1', 'numpy==1.23.1', 'matplotlib==3.5.2',
                         'pyqt5==5.15.7'])
